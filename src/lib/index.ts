eda5d24d3799135a124ab428543e83a30c12cfb9
JiKl - Jiří Klatovský <jirka@jikl-coding.com>
2025-06-17 12:13:20 +0200

    oprava exportu

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
 export * from "./seo/generateMetadata";
+export * from "./seo/getLocalBusinessStructuredData";
+export * from "./seo/getWebsiteStructuredData";
+export * from "./seo/getPersonStructuredData";
 export * from "./hooks/useDebounce";
